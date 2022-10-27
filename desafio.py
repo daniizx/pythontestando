845607969d6e95654fc7692a2bc53f217d8276a2
daniizx <89271070+daniizx@users.noreply.github.com>
2022-10-27 17:24:03 -0300

    Add files via upload

---
+++ desafio.py
@@ -0,0 +1,10 @@
+ # n1 = int(input('Digite um valor: '))
+ # n2 = int(input('Digite outro valor: '))
+ # r = n1+n2
+ # print('A soma vale', r)
+
+n = float(input('Digite um valor: '))
+print(n)
+
+
+
---
+++ desafio8.py
@@ -0,0 +1,7 @@
+larg =  float(input('largura da parede: '))
+alt = float(input('Altura da parede: '))
+área = larg * alt
+print('Sua parede tem a dimensão de {}x{} e sua área é de {}m².'.format(larg, alt, área))
+tinta = área / 2
+print('Para pintar essa parede, voce precisara  de {}l de tinta.'.format(tinta))
+
